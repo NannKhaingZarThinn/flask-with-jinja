69024ec57b69ae807124a9ee5ce5c92e308207a3
NannKhaingZar <nannkhaingzathinn@gmail.com>
2018-06-27 20:25:33 +0630

    changged indexProfile html in file

--- mainProfile.py
+++ mainProfile.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, url_for
 myapp = Flask(__name__)
 @myapp .route("/")
 def hello():
-        return render_template("index.html")
+        return render_template("indexProfile.html")
 
 if __name__ == '__main__':
     myapp.run(debug=True)
